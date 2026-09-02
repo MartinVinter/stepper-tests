63439a0db7c6b445dfd9b1f1a1081ff021717484
MartinVinter <61094818+MartinVinter@users.noreply.github.com>
2026-09-02 21:13:38 +0200

    Update simpler.py

--- simpler.py
+++ simpler.py
@@ -14,9 +14,9 @@ GPIO.output(DIR, GPIO.HIGH)
 try:
     while True:
         GPIO.output(STEP, GPIO.HIGH)
-        time.sleep(0.001)
+        time.sleep(0.01)
         GPIO.output(STEP, GPIO.LOW)
-        time.sleep(0.001)
+        time.sleep(0.01)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
